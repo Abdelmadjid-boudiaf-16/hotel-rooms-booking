fe5f1a70cf0fa36aa054ac08b8299f8faf1c0b10
Abdelmadjid-boudiaf-16 <boudiafabdelmadjid16@gmail.com>
2024-11-07 17:41:04 +0100

    #

--- app/api/booking/book-room/route.ts
+++ app/api/booking/book-room/route.ts
@@ -1,7 +1,6 @@
+import { prisma } from "@/prisma";
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
 
 export async function POST(req: Request) {
   try {
